aefb4ad273747ae25f272080c5e3d8caf0838a33
Phoori Chantima <alfa.phoori@gmail.com>
2026-02-20 19:20:32 +0700

    Rename Python/examples/04_variables.py to examples/04_variables.py

---
+++ examples/04_variables.py
@@ -0,0 +1,8 @@
+my_name = "Thanakorn yarnguy"
+number1 = 30
+number2 = 25
+print("ฉันชื่อ", my_name)
+print("เลขตัวที่ 1 คือ", number1)
+print("เลขตัวที่ 2 คือ", number2)
+
+print("คนที่ 1 มีน้ำหนักเท่ากับ", number1, "คนที่ 2 มีน้ำหนักเท่ากับ", number2, "หน่วยเป็นกิโลกรัม")
